cde1c9b07400ea2a85cbf41103b4579dbe2b7cb1
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2022-05-29 12:44:49 +0100

    add react-router link

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,27 @@
+import { Link } from 'react-router-dom';
+import logo from './assest/gratis-logo.png';
+
 const Navbar = ()=> (
-  <div>
+  <header>
+    <div>
+      <img src={logo} className="logo" alt="logo"/>
+      <p>METRICKS</p>
+    </div>
     
-  </div>
+    <nav>
+      <ul>
+        <li>
+          <Link>ABOUT US</Link>
+        </li>
+        <li>
+          <Link>BLOG</Link>
+        </li>
+        <li className='last-list'>
+          <Link>CONTACT US</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
 )
+
+export default Navbar
