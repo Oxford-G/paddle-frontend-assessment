33c30f8b0df8c54fea8124d4eb711abd41c8a517
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2022-05-31 17:15:55 +0100

    update blog.css file

--- src/Blog.js
+++ src/Blog.js
@@ -1,7 +1,17 @@
+import './Blog.css';
 
 const Blog = () => (
-  <div>
-    
+  <div className="Blog-container">
+    <div className="Blog-header">
+      <div className="Blog-blur"></div>
+      <div className="Blog-blur2"></div>
+      <p>
+        <span>------</span> BLOG
+      </p>
+      <h2> Articles and News</h2>
+    </div>
+    <div></div>
+    <div></div>
   </div>
 )
 
